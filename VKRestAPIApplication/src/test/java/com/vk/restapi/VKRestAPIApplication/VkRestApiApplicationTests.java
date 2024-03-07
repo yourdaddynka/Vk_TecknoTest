2f2effd975be31794f0d71e967cb36178dfc27fd
yourdaddynka <azat541@mail.ru>
2024-03-07 13:56:25 +0300

    start

---
+++ VKRestAPIApplication/src/test/java/com/vk/restapi/VKRestAPIApplication/VkRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.restapi.VKRestAPIApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VkRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
